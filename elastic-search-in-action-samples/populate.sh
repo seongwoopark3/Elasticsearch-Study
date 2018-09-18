00ecd74781bc3373e855e6f62f60a54140d6cab4
Seongwoo Park <seongwoopark3@gmail.com>
2018-09-18 15:25:09 +0900

    <fix> example data syntax for 6.x version

--- elastic-search-in-action-samples/populate.sh
+++ elastic-search-in-action-samples/populate.sh
@@ -25,7 +25,7 @@ curl -H "Content-Type: application/json" -s -XDELETE "$ADDRESS/get-together" > /
 
 # Create the next index using mapping.json
 echo "Creating 'get-together' index..."
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together" -d@$(dirname $0)/mapping.json
+curl -H "Content-Type: application/json" -s -XPUT "$ADDRESS/get-together" -d@$(dirname $0)/mapping.json
 
 # Wait for index to become yellow
 curl -H "Content-Type: application/json" -s "$ADDRESS/get-together/_health?wait_for_status=yellow&timeout=10s" > /dev/null
@@ -36,58 +36,63 @@ echo
 echo "Indexing data..."
 
 echo "Indexing groups..."
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/group/1" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/1?refresh" -d'{
   "name": "Denver Clojure",
   "organizer": ["Daniel", "Lee"],
   "description": "Group of Clojure enthusiasts from Denver who want to hack on code together and learn more about Clojure",
   "created_on": "2012-06-15",
   "tags": ["clojure", "denver", "functional programming", "jvm", "java"],
   "members": ["Lee", "Daniel", "Mike"],
-  "location_group": "Denver, Colorado, USA"
+  "location_group": "Denver, Colorado, USA",
+  "join_field": "group"
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/group/2" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/2?refresh" -d'{
   "name": "Elasticsearch Denver",
   "organizer": "Lee",
   "description": "Get together to learn more about using Elasticsearch, the applications and neat things you can do with ES!",
   "created_on": "2013-03-15",
   "tags": ["denver", "elasticsearch", "big data", "lucene", "solr"],
   "members": ["Lee", "Mike"],
-  "location_group": "Denver, Colorado, USA"
+  "location_group": "Denver, Colorado, USA",
+  "join_field": "group"
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/group/3" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/3?refresh" -d'{
   "name": "Elasticsearch San Francisco",
   "organizer": "Mik",
   "description": "Elasticsearch group for ES users of all knowledge levels",
   "created_on": "2012-08-07",
   "tags": ["elasticsearch", "big data", "lucene", "open source"],
   "members": ["Lee", "Igor"],
-  "location_group": "San Francisco, California, USA"
+  "location_group": "San Francisco, California, USA",
+  "join_field": "group"
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/group/4" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/4?refresh" -d'{
   "name": "Boulder/Denver big data get-together",
   "organizer": "Andy",
   "description": "Come learn and share your experience with nosql & big data technologies, no experience required",
   "created_on": "2010-04-02",
   "tags": ["big data", "data visualization", "open source", "cloud computing", "hadoop"],
   "members": ["Greg", "Bill"],
-  "location_group": "Boulder, Colorado, USA"
+  "location_group": "Boulder, Colorado, USA",
+  "join_field": "group"
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/group/5" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/5?refresh" -d'{
   "name": "Enterprise search London get-together",
   "organizer": "Tyler",
   "description": "Enterprise search get-togethers are an opportunity to get together with other people doing search.",
   "created_on": "2009-11-25",
   "tags": ["enterprise search", "apache lucene", "solr", "open source", "text analytics"],
   "members": ["Clint", "James"],
-  "location_group": "London, England, UK"
+  "location_group": "London, England, UK",
+  "join_field": "group"
 }'
 
 echo
@@ -95,7 +100,7 @@ echo "Done indexing groups."
 
 echo "Indexing events..."
 
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/100?parent=1" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/100?routing=1&refresh" -d'{
   "host": ["Lee", "Troy"],
   "title": "Liberator and Immutant",
   "description": "We will discuss two different frameworks in Clojure for doing different things. Liberator is a ring-compatible web framework based on Erlang Webmachine. Immutant is an all-in-one enterprise application based on JBoss.",
@@ -105,10 +110,14 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Stoneys Full Steam Tavern",
     "geolocation": "39.752337,-105.00083"
   },
-  "reviews": 4
+  "reviews": 4,
+  "join_field": {
+    "name": "event",
+    "parent": "1"
+  }
 }'
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/101?parent=1" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/101?routing=1&refresh" -d'{
   "host": "Sean",
   "title": "Sunday, Surly Sunday",
   "description": "Sort out any setup issues and work on Surlybird issues. We can use the EC2 node as a bounce point for pairing.",
@@ -117,11 +126,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
   "location_event": {
     "name": "IRC, #denofclojure"
   },
-  "reviews": 2
+  "reviews": 2,
+  "join_field": {
+    "name": "event",
+    "parent": "1"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/102?parent=1" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/102?routing=1&refresh" -d'{
   "host": "Daniel",
   "title": "10 Clojure coding techniques you should know, and project openbike",
   "description": "What are ten Clojure coding techniques that you wish everyone knew? We will also check on the status of Project Openbike.",
@@ -131,11 +144,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Stoneys Full Steam Tavern",
     "geolocation": "39.752337,-105.00083"
   },
-  "reviews": 3
+  "reviews": 3,
+  "join_field": {
+    "name": "event",
+    "parent": "1"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/103?parent=2" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/103?routing=1&refresh" -d'{
   "host": "Lee",
   "title": "Introduction to Elasticsearch",
   "description": "An introduction to ES and each other. We can meet and greet and I will present on some Elasticsearch basics and how we use it.",
@@ -145,11 +162,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Stoneys Full Steam Tavern",
     "geolocation": "39.752337,-105.00083"
   },
-  "reviews": 5
+  "reviews": 5,
+  "join_field": {
+    "name": "event",
+    "parent": "2"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/104?parent=2" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/104?routing=1&refresh" -d'{
   "host": "Lee",
   "title": "Queries and Filters",
   "description": "A get together to talk about different ways to query Elasticsearch, what works best for different kinds of applications.",
@@ -159,11 +180,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Stoneys Full Steam Tavern",
     "geolocation": "39.752337,-105.00083"
   },
-  "reviews": 1
+  "reviews": 1,
+  "join_field": {
+    "name": "event",
+    "parent": "2"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/105?parent=2" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/105?routing=1&refresh" -d'{
   "host": "Lee",
   "title": "Elasticsearch and Logstash",
   "description": "We can get together and talk about Logstash - http://logstash.net with a sneak peek at Kibana",
@@ -173,11 +198,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Stoneys Full Steam Tavern",
     "geolocation": "39.752337,-105.00083"
   },
-  "reviews": null
+  "reviews": null,
+  "join_field": {
+    "name": "event",
+    "parent": "2"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/106?parent=3" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/106?routing=1&refresh" -d'{
   "host": "Mik",
   "title": "Social management and monitoring tools",
   "description": "Shay Banon will be there to answer questions and we can talk about management tools.",
@@ -187,11 +216,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Quid Inc",
     "geolocation": "37.798442,-122.399801"
   },
-  "reviews": 5
+  "reviews": 5,
+  "join_field": {
+    "name": "event",
+    "parent": "3"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/107?parent=3" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/107?routing=1&refresh" -d'{
   "host": "Mik",
   "title": "Logging and Elasticsearch",
   "description": "Get a deep dive for what Elasticsearch is and how it can be used for logging with Logstash as well as Kibana!",
@@ -201,11 +234,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Salesforce headquarters",
     "geolocation": "37.793592,-122.397033"
   },
-  "reviews": 3
+  "reviews": 3,
+  "join_field": {
+    "name": "event",
+    "parent": "3"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/108?parent=3" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/108?routing=1&refresh" -d'{
   "host": "Elyse",
   "title": "Piggyback on Elasticsearch training in San Francisco",
   "description": "We can piggyback on training by Elasticsearch to have some Q&A time with the ES devs",
@@ -215,11 +252,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "NoSQL Roadshow",
     "geolocation": "37.787742,-122.398964"
   },
-  "reviews": 5
+  "reviews": 5,
+  "join_field": {
+    "name": "event",
+    "parent": "3"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/109?parent=4" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/109?routing=1&refresh" -d'{
   "host": "Andy",
   "title": "Hortonworks, the future of Hadoop and big data",
   "description": "Presentation on the work that hortonworks is doing on Hadoop",
@@ -229,11 +270,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "SendGrid Denver office",
     "geolocation": "39.748477,-104.998852"
   },
-  "reviews": 2
+  "reviews": 2,
+  "join_field": {
+    "name": "event",
+    "parent": "4"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/110?parent=4" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/110?routing=1&refresh" -d'{
   "host": "Andy",
   "title": "Big Data and the cloud at Microsoft",
   "description": "Discussion about the Microsoft Azure cloud and HDInsight.",
@@ -243,11 +288,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Bing Boulder office",
     "geolocation": "40.018528,-105.275806"
   },
-  "reviews": 1
+  "reviews": 1,
+  "join_field": {
+    "name": "event",
+    "parent": "4"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/111?parent=4" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/111?routing=1&refresh" -d'{
   "host": "Andy",
   "title": "Moving Hadoop to the mainstream",
   "description": "Come hear about how Hadoop is moving to the main stream",
@@ -257,11 +306,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Courtyard Boulder Louisville",
     "geolocation": "39.959409,-105.163497"
   },
-  "reviews": 4
+  "reviews": 4,
+  "join_field": {
+    "name": "event",
+    "parent": "4"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/112?parent=5" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/112?routing=1&refresh" -d'{
   "host": "Dave Nolan",
   "title": "real-time Elasticsearch",
   "description": "We will discuss using Elasticsearch to index data in real time",
@@ -271,11 +324,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "SkillsMatter Exchange",
     "geolocation": "51.524806,-0.099095"
   },
-  "reviews": 3
+  "reviews": 3,
+  "join_field": {
+    "name": "event",
+    "parent": "5"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/113?parent=5" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/113?routing=1&refresh" -d'{
   "host": "Dave",
   "title": "Elasticsearch at Rangespan and Exonar",
   "description": "Representatives from Rangespan and Exonar will come and discuss how they use Elasticsearch",
@@ -285,11 +342,15 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "Alumni Theatre",
     "geolocation": "51.51558,-0.117699"
   },
-  "reviews": 3
+  "reviews": 3,
+  "join_field": {
+    "name": "event",
+    "parent": "5"
+  }
 }'
 
 echo
-curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/114?parent=5" -d'{
+curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/_doc/114?routing=1&refresh" -d'{
   "host": "Yann",
   "title": "Using Hadoop with Elasticsearch",
   "description": "We will walk through using Hadoop with Elasticsearch for big data crunching!",
@@ -299,7 +360,11 @@ curl -H "Content-Type: application/json" -s -XPOST "$ADDRESS/get-together/event/
     "name": "SkillsMatter Exchange",
     "geolocation": "51.524806,-0.099095"
   },
-  "reviews": 2
+  "reviews": 2,
+  "join_field": {
+    "name": "event",
+    "parent": "5"
+  }
 }'
 
 echo
